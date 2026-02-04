f9692cfde190600344fd01cb25331514a05e4375
Biswajit Rout <routhappy9876@gmail.com>
2026-02-04 06:51:20 +0530

    update project-4

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -1,24 +1,133 @@
-import React from 'react'
-import { Bookmark } from 'lucide-react'
 import Card from './components/Card.jsx'
-import User from './components/User.jsx'
+
 
 const App = () => {
 
-  const arr = [10, 20, 30, 40];
+  const jobOpenings = [
+  {
+    brandLogo: "https://cdn.vectorstock.com/i/1000v/47/20/google-logo-vector-50014720.jpg",
+    companyName: "Google",
+    datePosted: "5 days ago",
+    post: "Frontend Software Engineer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: 45, // dollars per hour
+    location: "Bangalore, India"
+  },
+  {
+    brandLogo: "https://1000logos.net/wp-content/uploads/2016/10/Amazon-logo-meaning.jpg",
+    companyName: "Amazon",
+    datePosted: "2 weeks ago",
+    post: "Backend Developer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: 50,
+    location: "Hyderabad, India"
+  },
+  {
+    brandLogo: "https://1000logos.net/wp-content/uploads/2016/10/Apple-Logo.png",
+    companyName: "Apple",
+    datePosted: "1 week ago",
+    post: "iOS Developer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: 65,
+    location: "Bangalore, India"
+  },
+  {
+    brandLogo: "https://cdn.pixabay.com/photo/2021/12/14/22/29/meta-6871457_1280.png",
+    companyName: "Meta",
+    datePosted: "3 days ago",
+    post: "React Developer",
+    tag1: "Remote",
+    tag2: "Mid Level",
+    pay: 60,
+    location: "Remote, India"
+  },
+  {
+    brandLogo: "https://images.ctfassets.net/y2ske730sjqp/1aONibCke6niZhgPxuiilC/2c401b05a07288746ddf3bd3943fbc76/BrandAssets_Logos_01-Wordmark.jpg?w=940",
+    companyName: "Netflix",
+    datePosted: "10 days ago",
+    post: "UI Engineer",
+    tag1: "Full Time",
+    tag2: "Senior Level",
+    pay: 70,
+    location: "Mumbai, India"
+  },
+  {
+    brandLogo: "https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/RWCZER-Legal-IP-Trademarks-CP-MS-logo-740x417-1?wid=406&hei=230&fit=crop&resSharp=1",
+    companyName: "Microsoft",
+    datePosted: "4 weeks ago",
+    post: "Full Stack Developer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: 48,
+    location: "Noida, India"
+  },
+  {
+    brandLogo: "https://download.logo.wine/logo/Uber/Uber-Logo.wine.png",
+    companyName: "Uber",
+    datePosted: "6 days ago",
+    post: "Software Engineer",
+    tag1: "Contract",
+    tag2: "Mid Level",
+    pay: 55,
+    location: "Hyderabad, India"
+  },
+  {
+    brandLogo: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/960px-Airbnb_Logo_B%C3%A9lo.svg.png",
+    companyName: "Airbnb",
+    datePosted: "2 months ago",
+    post: "Frontend Engineer",
+    tag1: "Remote",
+    tag2: "Senior Level",
+    pay: 68,
+    location: "Remote, India"
+  },
+  {
+    brandLogo: "https://www.freepnglogos.com/uploads/tesla-logo-png-black-1.png",
+    companyName: "Tesla",
+    datePosted: "3 weeks ago",
+    post: "Software Engineer",
+    tag1: "Full Time",
+    tag2: "Mid Level",
+    pay: 58,
+    location: "Pune, India"
+  },
+  {
+    brandLogo: "https://download.logo.wine/logo/IBM/IBM-Logo.wine.png",
+    companyName: "IBM",
+    datePosted: "8 days ago",
+    post: "Java Developer",
+    tag1: "Full Time",
+    tag2: "Junior Level",
+    pay: 42,
+    location: "Kolkata, India"
+  }
+];
+
+// console.log(jobOpenings);
+
+
 
   return (
     <div className='parent'>
-      {/* <User name={arr[0]} />
-      <User name={arr[1]} />
-      <User name={arr[2]} /> */}
-
-      {arr.map(function () {
-        return 'hello'
+      {jobOpenings.map(function(elem, idx) {
+        console.log(idx);
+        
+        // return <h1>Hey</h1>
+        // return <h1>{elem.pay}</h1>
+        // return <h1>{elem.companyName}</h1>
+        return <div key={idx}> 
+        <Card company={elem.companyName} datePosted={elem.datePosted} post={elem.post} tag1={elem.tag1} brandLogo={elem.brandLogo} pay={elem.pay} tag2={elem.tag2} />
+        </div>
       })}
     </div>
   )
 }
 
 export default App
+
+
+
 
--- 04-cards-project/src/components/Card.jsx
+++ 04-cards-project/src/components/Card.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import {Bookmark} from 'lucide-react'
 
-function Card() {
+const Card = (props) => {
+  
+  //src="https://1000logos.net/wp-content/uploads/2016/10/Amazon-logo-meaning.jpg"
+
   return (
     <div>
         <div className='card'>
        <div>
          <div className="top">
-          <img src="https://1000logos.net/wp-content/uploads/2016/10/Amazon-logo-meaning.jpg" alt="" />
+          <img src={props.brandLogo} alt="Logo" />
           <button>Save <Bookmark size={12} /> </button>
         </div>
         <div className='center'>
-          <h3>Amazon <span>5 days ago</span></h3>
-          <h2>Senior UI/UX Designer</h2>
+          <h3>{props.company} <span>{props.datePosted}</span></h3>
+          <h2>{props.post}</h2>
           <div className='tag'>
-            <h4>Part Time</h4>
-            <h4>Senior Level</h4>
+            <h4>{props.tag1}</h4>
+            <h4>{props.tag2}</h4>
           </div>
         </div>
        </div>
         <div className="bottom">
           <div>
-            <h3>$120/hr</h3>
+            <h3>${props.pay}</h3>
             <h3>Mumbai, India</h3>
           </div>
           <button>Apply Now</button>
