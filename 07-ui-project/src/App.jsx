e93f1cf07ed3f747e98203c343179a5b083ae621
Biswajit Rout <routhappy9876@gmail.com>
2026-02-05 21:50:56 +0530

    updates all files

--- 07-ui-project/src/App.jsx
+++ 07-ui-project/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import Section1 from './components/Section1/Section1'
+import Section2 from './components/Section2/Section2'
 
 const App = () => {
   return (
-    <div >
+    <div>
+      <Section1 />
+      <Section2 /> 
     </div>
   )
 }
 
 export default App
+
+
+
+
 
---
+++ 07-ui-project/src/components/Section1/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      This is Navbar
+    </div>
+  )
+}
+
+export default Navbar
+
+
