a18d1ed3ce9e7a30d223cdbbc45d171bdcf6669d
Biswajit Rout <routhappy9876@gmail.com>
2026-02-06 07:48:15 +0530

    update in 07-ui-project

---
+++ 07-ui-project/src/components/Section1/HeroText.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroText = () => {
+    return (
+        <div className='p-6'>
+            <h3 className='mb-7 leading-[1.1] text-7xl font-bold'>Prospective <br />
+                <span>customer</span> <br /> Segmentation</h3>
+            <p className='text-xl font-medium text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ipsum similique eos nam debitis ea exercitationem explicabo mollitia doloribus odit.</p>
+        </div>
+    )
+}
+
+export default HeroText
---
+++ 07-ui-project/src/components/Section1/Page1Content.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import LeftContent from './LeftContent'
+import RightContent from './RightContent'
+
+const Page1Content = () => {
+  return (
+    <div className='py-3 px-18 h-[90vh] flex justify-between items-center gap-10'>
+      <LeftContent />
+      <RightContent />
+    </div>
+  )
+}
+
+export default Page1Content
+
+
+
+
+
