15ee71e37eb846b7633d0a6cab8f5ffeeab014ab
Biswajit Rout <routhappy9876@gmail.com>
2026-02-08 17:26:57 +0530

    update prevoius 09-useState

--- 09-useState/src/App.jsx
+++ 09-useState/src/App.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [num, setNum] = useState(0)
+
+  function increaseNum() {
+    setNum(prev => prev + 1)
+  }
+
+  function decreasingNum() {
+    setNum(prev => prev - 1)
+  }
+
+  function jump5Num() {
+    setNum(prev => prev + 5)
+  }
+
   return (
-    <div>
-      <h1>0</h1>
-      <button>Increase</button>
-      <button>Decrease</button>
+    <div className="container">
+      <div className="card">
+        <h1>{num}</h1>
+        <div className="btns">
+          <button onClick={increaseNum}>Increase</button>
+          <button onClick={decreasingNum}>Decrease</button>
+          <button onClick={jump5Num}>Increase by 5</button>
+        </div>
+      </div>
     </div>
   )
 }
 
 export default App
-
-
-
-
-
-
