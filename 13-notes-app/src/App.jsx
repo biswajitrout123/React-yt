a0ddd344483a480a994f10ace79cca8d44b47688
Biswajit Rout <routhappy9876@gmail.com>
2026-02-11 12:37:47 +0530

    update previous and added notes-app

--- 13-notes-app/src/App.jsx
+++ 13-notes-app/src/App.jsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+
+  const [task, setTask] = useState([])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+
+
+    // console.log(title,details);
+    const copyTask = [...task];
+
+    copyTask.push({title, details})
+    
+    setTask(copyTask)
+    
+
+    setTitle('')
+
+    setDetails('')
+
+  }
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className='h-screen lg:flex bg-black text-white'>
+      <form onSubmit={(e) => {
+        submitHandler(e)
+      }} className='flex gap-4 lg:w-1/2 p-10 flex-col items-start'>
+        <h1 className='text-3xl font-bold'>Add Notes</h1>
+
+        {/* PEHLA INPUT FOR HEADING */}
+        <input type="text" placeholder='Enter Notes Heading'
+          className='px-5 w-full font-medium py-2 border-2 outline-none rounded'
+          value={title}
+
+          onChange={(e) => {
+            setTitle(e.target.value)
+
+          }}
+        />
+
+        {/* DETAILED WALA INPUT */}
+        <textarea type="text" placeholder='Write Details'
+          className='px-5 w-full font-medium h-32 py-2 flex items-start flex-row border-2 outline-none rounded'
+          value={details}
+          onChange={(e) => {
+            setDetails(e.target.value)
+          }}
+        />
+
+        <button
+          className='bg-white active:scale-95 font-medium outline-none w-full text-black px-5 py-2 rounded'
+          >
+          Add Noes
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      </form>
+
+      <div className='lg:w-1/2 lg:border-l-2 p-10'>
+        <h1 className='text-4xl font-bold'>Recent Notes</h1>
+        <div className='flex flex-wrap items-start justify-start gap-5 mt-5 h-full overflow-auto'>
+          {task.map(function(elem, idx) {
+
+            return <div key={idx} className='h-52 w-40  text-black p-4 rounded-xl bg-[url("https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png")]'>
+              <h3 className='leading-tight text-xl font-bold'>{elem.title}</h3>
+              <p className='mt-2 leading-tight font-medium text-gray-700'>{elem.details}</p>
+
+            </div>
+          })}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
 export default App
+
+
+
+
+
