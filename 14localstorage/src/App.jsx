0ccc5fb4a3dd5d2f193227d7186d4769c855301d
Biswajit Rout <routhappy9876@gmail.com>
2026-02-12 09:04:53 +0530

    added 14-localStorage

---
+++ 14localstorage/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const App = () => {
+
+  // CLEAR THE LOCAL STORAGE:
+  // localStorage.clear()
+
+  // Clear the SESSION STORAGE:
+  // sessionStorage.clear()
+
+  
+
+  return (
+    <div>
+      App
+      hay
+    </div>
+  )
+}
+
+export default App
