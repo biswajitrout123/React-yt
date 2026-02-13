855e003cedd5f0ebf3875561b5c5ba86918bb819
Biswajit Rout <routhappy9876@gmail.com>
2026-02-13 17:30:10 +0530

    update gallery project

--- 17-gallery-project/src/App.jsx
+++ 17-gallery-project/src/App.jsx
@@ -1,41 +1,59 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const App = () => {
 
   const [userData, setUserData] = useState([])
 
-  const getData = async() => {
+  const getData = async () => {
 
-    const response = await axios.get('https://picsum.photos/v2/list?page=4&limit=15')
+    const response = await axios.get('https://picsum.photos/v2/list?page=4&limit=30')
     setUserData(response.data)
-    console.log(response.data);
-    
-    
+
   }
 
-  let printUserData = 'No User Available'
+  useEffect(function() {
+    getData()
+  }, [])
 
-  if(userData.length > 0) {
-    printUserData = userData.map(function() {
-      return 'hello'
+
+  let printUserData = <h3 className='text-gray-400 text-xs'>No User Available</h3>
+
+  if (userData.length > 0) {
+    printUserData = userData.map(function (elem, idx) {
+
+      return <div key={idx}>
+        <a href={elem.url} target='_blank'>
+          <div className='h-40 w-44 overflow-hidden bg-white rounded-xl'>
+            <img className='h-full w-full object-cover' src={elem.download_url} alt="" />
+          </div>
+          <h2 className='font-bold text-lg'>
+            {elem.author}
+          </h2>
+
+        </a>
+      </div>
     })
   }
 
   return (
-    <div className='bg-black h-screen p-4 text-white'>
-      <button onClick={getData}
-        className='bg-green-600 active:scale-95 mb-3 px-5 py-2 rounded text-white'>Get data</button>
+    <div className='bg-black overflow-auto h-screen p-4 text-white'>
+      
 
-        <div>
-          {printUserData}
-
-        </div>
+      <div className='flex flex-wrap gap-4 p-2'>
+        {printUserData}
+      </div>
+      <div className='flex justify-center gap-6 items-center p-4'>
+        <button className='bg-amber-400 text-sm  cursor-pointer active:scale-95 text-black rounded px-4 py-2 font-semibold'>Prev</button>
+        <button className='bg-amber-400 text-sm  cursor-pointer active:scale-95 text-black rounded px-4 py-2 font-semibold'>Next</button>
+      </div>
     </div>
   )
 }
 
 export default App
+
+
 
 
 
