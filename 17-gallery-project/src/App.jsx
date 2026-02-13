fff653f02bbef61648aa6af1dcfd378a20e383e5
Biswajit Rout <routhappy9876@gmail.com>
2026-02-13 13:17:37 +0530

    update gallery-project

--- 17-gallery-project/src/App.jsx
+++ 17-gallery-project/src/App.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const App = () => {
+
+  const [userData, setUserData] = useState([])
+
+  const getData = async() => {
+
+    const response = await axios.get('https://picsum.photos/v2/list?page=4&limit=15')
+    setUserData(response.data)
+    console.log(response.data);
+    
+    
+  }
+
+  let printUserData = 'No User Available'
+
+  if(userData.length > 0) {
+    printUserData = userData.map(function() {
+      return 'hello'
+    })
+  }
+
   return (
-    <div>
-      
+    <div className='bg-black h-screen p-4 text-white'>
+      <button onClick={getData}
+        className='bg-green-600 active:scale-95 mb-3 px-5 py-2 rounded text-white'>Get data</button>
+
+        <div>
+          {printUserData}
+
+        </div>
     </div>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
