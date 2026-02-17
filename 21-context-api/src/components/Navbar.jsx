2a26a5c8ba440a8d52bf55e4a235ad0f7a5bc473
Biswajit Rout <routhappy9876@gmail.com>
2026-02-17 08:02:43 +0530

    upate 21-context-api

---
+++ 21-context-api/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav2 from './Nav2'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+      <h2>Sheryians</h2>
+      <Nav2 />
+    </div>
+  )
+}
+
+export default Navbar
