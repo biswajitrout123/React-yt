9b6cedcec755cfb2b5a6a8eafebb43e1799ca974
Biswajit Rout <routhappy9876@gmail.com>
2026-02-17 14:00:07 +0530

    update this

--- 21-context-api/src/components/Navbar.jsx
+++ 21-context-api/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Nav2 from './Nav2'
 
-const Navbar = () => {
+const Navbar = (props) => {
   return (
     <div className='nav'>
       <h2>Sheryians</h2>
-      <Nav2 />
+      <Nav2 theme={props.theme}/>
     </div>
   )
 }
