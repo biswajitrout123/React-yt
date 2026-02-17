58eebca613b23e43063dd9337622c8eb2a3e8148
Biswajit Rout <routhappy9876@gmail.com>
2026-02-17 06:16:46 +0530

    Add and Complete Bonus-Topic

---
+++ 20-bonus-topic/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = (props) => {
+    console.log(props);
+
+    function changeTheme() {
+        props.setTheme('dark')
+        
+    }
+
+  return (
+    <div>
+      <button onClick={changeTheme}>Change Theme</button>
+    </div>
+  )
+}
+
+export default Navbar
